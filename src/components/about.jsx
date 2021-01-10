23130c5f0394ff73ee1e621e14972022ecc347ac
delphi1004 <delphi1004@hanmail.net>
2021-01-10 21:47:41 +0900

    modified about

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -30,15 +30,35 @@
 import React from 'react'
 import './about.css'
 
+// const About = () => {
+
+//   return (
+//     <div id='aboutContainer'>
+//       <h1 id='aboutTitle'>I am a Creative technologist, New media artist</h1>
+//       <div id='aboutDescription'>
+//         <p >
+//           with mobile dev and media art experience, wanting to focus on my skills more on creative projects. I have over a decade of professional programming experience (C#, C++, C, Objective-C). I have experienced with Unity3D and computer graphics(shader coding, OpenGL, graphics pipeline). I have also made interactive art installations and released a mobile app on iOS. I want to combine all of those skills to help make a great creative project. I am looking for creative projects and want to work with passionate people.
+//         </p>
+//       </div>
+//     </div >
+//   )
+// }
+
 const About = () => {
+  const image = require('../data/images/originalSize/about.JPG')
 
   return (
     <div id='aboutContainer'>
-      <h1 id='aboutTitle'>I am a Creative technologist, New media artist</h1>
-      <div id='aboutDescription'>
-        <p >
-          with mobile dev and media art experience, wanting to focus on my skills more on creative projects. I have over a decade of professional programming experience (C#, C++, C, Objective-C). I have experienced with Unity3D and computer graphics(shader coding, OpenGL, graphics pipeline). I have also made interactive art installations and released a mobile app on iOS. I want to combine all of those skills to help make a great creative project. I am looking for creative projects and want to work with passionate people.
-        </p>
+      <div id='about-image-container'>
+        <img id='about-image' src={image.default} alt='image' />
+        <p className='about-image-description'>Lightning talks in VK19</p>
+        <a className='about-image-description' href='https://vizknowledge.aalto.fi/archive/2019/' target='_blank'> https://vizknowledge.aalto.fi/archive/2019/</a>
+        <p className='about-image-description'>Photo by Iiro Immonen</p>
+      </div>
+      <div id='about-description'>
+        <p id='about-title'>JOHN LEE</p>
+        <p id='about-sub-title'>A Creative technologist, New media artist</p>
+        <p id='about-text'>I have over a decade of professional programming experience. I have also made interactive art installations and released a mobile app on iOS. I want to combine all of those skills to help make a great creative project. I am looking for creative projects and want to work with passionate people.</p>
       </div>
     </div >
   )
