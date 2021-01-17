1deaaa6b4f0c70bf44c6bb5b804d405a4c4394dd
delphi1004 <delphi1004@hanmail.net>
2021-01-17 20:27:12 +0900

    modified test

--- test/contents.jest.spec.jsx
+++ test/contents.jest.spec.jsx
@@ -7,27 +7,29 @@ import configureStore from 'redux-mock-store'
 import Title from '../src/components/title'
 import Header from '../src/components/header'
 
-test('renders title', () => {
-  const component = render(
-    <Title />
-  )
-  expect(component.container).toHaveTextContent(
-    'John Lee'
-  )
-})
+describe('check the contents', () => {
+  test('renders title', () => {
+    const component = render(
+      <Title />
+    )
+    expect(component.container).toHaveTextContent(
+      'John Lee'
+    )
+  })
 
-test('renders menu', () => {
-  const mockStore = configureStore()
-  let store
+  test('renders menu', () => {
+    const mockStore = configureStore()
+    let store
 
-  store = mockStore(null)
-  const component = render(
-    <Router>
-      <Provider store={store}><Header /></Provider>
-    </Router>
-  )
-  expect(component.container).toHaveTextContent('about')
-  expect(component.container).toHaveTextContent('work')
-  expect(component.container).toHaveTextContent('exhibition')
-  expect(component.container).toHaveTextContent('contact')
+    store = mockStore(null)
+    const component = render(
+      <Router>
+        <Provider store={store}><Header /></Provider>
+      </Router>
+    )
+    expect(component.container).toHaveTextContent('about')
+    expect(component.container).toHaveTextContent('work')
+    expect(component.container).toHaveTextContent('exhibition')
+    expect(component.container).toHaveTextContent('contact')
+  })
 })
