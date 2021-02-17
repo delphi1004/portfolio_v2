3ca4fbdcda089879edb66a4cf2cf3b0880cd48f4
delphi1004 <delphi1004@hanmail.net>
2021-02-17 11:19:47 +0900

    Refactoring for reducer, based on Jakousa's comment

--- src/store.js
+++ src/store.js
@@ -3,10 +3,8 @@ import thunk from 'redux-thunk'
 // import { composeWithDevTools } from 'redux-devtools-extension'
 import statusReducer from './reducer/statusReducer'
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-
-
-// const composeEnhancers =  compose
+//const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const composeEnhancers =  compose
 
 const reducer = combineReducers({
   systemStatus:statusReducer
